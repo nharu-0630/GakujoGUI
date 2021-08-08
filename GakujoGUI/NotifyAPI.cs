ff791222e6d8b05ee3f39cbd687bc2a0866cad2a
Yuto Sahashi <sahashi2002@gmail.com>
2021-08-08 22:11:02 +0900

    Add Discord Todoist

---
+++ GakujoGUI/NotifyAPI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Todoist.Net;
+
+namespace GakujoGUI
+{
+    class NotifyAPI
+    {
+        private DiscordSocketClient discordSocketClient;
+        private ulong discordChannelId;
+        private ITodoistClient todoistClient;
+
+        public async void LoginDiscord(string token, ulong channel)
+        {
+            discordSocketClient = new DiscordSocketClient();
+            await discordSocketClient.LoginAsync(TokenType.Bot, token);
+            await discordSocketClient.StartAsync();
+            discordChannelId = channel;
+        }
+
+
+        public async void NotifyDiscord(string message)
+        {
+            await (discordSocketClient.GetChannel(discordChannelId) as IMessageChannel).SendMessageAsync(message);
+        }
+
+        private void LoginTodoist(string token)
+        {
+            todoistClient = new TodoistClient(token);
+        }
+
+        private async void AddTodoistTask(string message)
+        {
+            await todoistClient.Items.QuickAddAsync(new Todoist.Net.Models.QuickAddItem(message));
+        }
+    }
+}
